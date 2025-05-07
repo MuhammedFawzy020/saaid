f4d1d11b162d4f70bae93cb687f1680958a48689
Ahmed Wael <30728647+AhmedWael97@users.noreply.github.com>
2025-05-08 01:00:12 +0300

    Update app.blade.php

--- resources/views/frontend_v2/layout/app.blade.php
+++ resources/views/frontend_v2/layout/app.blade.php
@@ -7,19 +7,25 @@ $setting = App\Models\Setting::first();
 ?>
 
 <head>
-    <script async src="https://www.googletagmanager.com/gtag/js?id=AW-11407152342"></script>
-    <script>
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-    
-      gtag('config', 'AW-11407152342');
-    </script>
+   <!-- Google Tag Manager -->
+<script>
+    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TWNGVDP8');
+</script>
+<!-- End Google Tag Manager -->
     @include('frontend_v2.layout.header')
 </head>
 
 
 <body>
+
+    <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TWNGVDP8"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
     
     <!-- Preloader -->
     <div class="preloader-bg"></div>
